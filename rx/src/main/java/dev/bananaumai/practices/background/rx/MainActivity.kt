e6a68df295ab15b240dba4cb75b2135bdf58f1e6
Yuta Shimakawa <bananaumai@tinkermode.com>
2019-05-01 03:07:16 +0900

    Rx way

--- rx/src/main/java/dev/bananaumai/practices/background/rx/MainActivity.kt
+++ rx/src/main/java/dev/bananaumai/practices/background/rx/MainActivity.kt
@@ -8,12 +8,16 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.IBinder
 import android.util.Log
+import io.reactivex.processors.PublishProcessor
+import io.reactivex.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
     private val tag = this.javaClass.name
 
+    private val processor = PublishProcessor.create<Any>()
+
     private var boundEmitter = false
-    private lateinit var emitter: DataEmitter
+    private var emitter: DataEmitter? = null
     private val emitterConnection = object : ServiceConnection {
         private val tag = this.javaClass.name
 
@@ -21,11 +25,30 @@ class MainActivity : AppCompatActivity() {
             Log.d(tag, "onServiceConnected")
             val binder = service as DataEmitter.LocalBinder
             emitter = binder.getService()
+            emitter?.startEmit(processor)
             boundEmitter = true
         }
 
         override fun onServiceDisconnected(name: ComponentName?) {
             boundEmitter = false
+        }
+    }
+
+    private var boundHandler = false
+    private var handler: DataHandler? = null
+    private val handlerConnection = object : ServiceConnection {
+        private val tag = this.javaClass.name
+
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            Log.d(tag, "onServiceConnected")
+            val binder = service as DataHandler.LocalBinder
+            handler = binder.getService()
+            handler?.startHandle(processor)
+            boundHandler = true
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?) {
+            boundHandler = false
         }
     }
 
@@ -36,9 +59,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         Intent(this, DataEmitter::class.java).also {
-            Log.d(tag, "before bindService")
             bindService(it, emitterConnection, Context.BIND_AUTO_CREATE)
         }
+
+        Intent(this, DataHandler::class.java).also {
+            bindService(it, handlerConnection, Context.BIND_AUTO_CREATE)
+        }
+
     }
 
     override fun onDestroy() {
--- rx/src/main/java/dev/bananaumai/practices/background/rx/Service.kt
+++ rx/src/main/java/dev/bananaumai/practices/background/rx/Service.kt
@@ -9,128 +9,74 @@ import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.os.*
 import android.util.Log
-import java.io.Serializable
 import kotlin.random.Random
+import io.reactivex.processors.*
+import io.reactivex.schedulers.Schedulers
 
-class DataProcessor : Service() {
+class DataHandler : Service() {
     private val tag = this.javaClass.name
-    private lateinit var serviceHandler: ServiceHandler
     private val rand = Random.Default
+    private val binder: LocalBinder = LocalBinder()
 
-    private inner class ServiceHandler(looper: Looper) : Handler(looper) {
-        override fun handleMessage(msg: Message?) {
-            val intent = msg?.obj as Intent?
-
-            val i = rand.nextInt(10)
-
-            if (i % 4 == 0) {
-                Thread.sleep(10)
-            }
-
-            if (intent != null) {
-                val data = intent.getStringExtra("data")
-                Log.d(tag, data)
-            }
-
-            Log.d(tag, "isIdle?: ${looper.queue.isIdle}")
-
-            val printer = { x: String? ->
-                Log.d(tag, x)
-                Unit
-            }
-
-            if (!looper.queue.isIdle) {
-                looper.dump(printer, "hoge")
-            }
-
-            super.handleMessage(msg)
-        }
-    }
-
-    override fun onCreate() {
-        HandlerThread("DataProcessorHandlerThread", Process.THREAD_PRIORITY_BACKGROUND).apply {
-            start()
-            serviceHandler = ServiceHandler(looper)
-        }
+    inner class LocalBinder : Binder() {
+        fun getService() = this@DataHandler
     }
 
     override fun onBind(intent: Intent?): IBinder? {
-        return null
+        return binder
     }
 
-    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        serviceHandler.obtainMessage().also { msg ->
-            msg.arg1 = startId
-            msg.obj = intent
-            serviceHandler.sendMessage(msg)
-        }
+    fun startHandle(processor: PublishProcessor<Any>) {
+        processor.observeOn(Schedulers.computation()).subscribe {
+            if (rand.nextInt(10) % 2 == 0) {
+                Thread.sleep(40)
+            }
 
-        return START_STICKY
+            Log.d(tag, "${Thread.currentThread().name}: " + it.toString())
+        }
     }
 }
 
 class DataEmitter : Service() {
-    private val tag = this.javaClass.name
-
     private val binder: LocalBinder = LocalBinder()
 
     inner class LocalBinder : Binder() {
         fun getService() = this@DataEmitter
     }
 
-    override fun onCreate() {
-        Log.d(tag, "onCreate")
-
-        super.onCreate()
-
-        val dispatcher = { data: Any ->
-            Intent(this, DataProcessor::class.java).also {
-                it.putExtra("data", data.toString())
-                startService(it)
-            }
-            Unit
-        }
-
-        val accelerometer = Accelerometer(getSystemService(Context.SENSOR_SERVICE) as SensorManager)
-        accelerometer.listen(dispatcher)
-    }
-
     override fun onBind(intent: Intent?): IBinder? {
-        Log.d(tag, "onBind")
         return binder
     }
 
-    override fun onDestroy() {
-        Log.d(tag, "onDestroy")
-
-        super.onDestroy()
+    fun startEmit(processor: PublishProcessor<Any>) {
+        val sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        AccelerometerEventEmitter.start(sensorManager, processor)
     }
 }
 
-interface EventListener {
-    fun listen(callback: (Any) -> Unit)
+class AccelerometerEvent(private val event: SensorEvent) {
+    override fun toString() =
+        "${this.javaClass.name} { timestamp: ${event.timestamp}, values: ${event.values.joinToString(",", "[", "]")} }"
 }
 
-class Accelerometer(private val manager: SensorManager) : EventListener {
-    inner class AccelerometerEvent(private val event: SensorEvent) : Serializable {
-        override fun toString() =
-            "${this.javaClass.name} { timestamp: ${event.timestamp}, values: ${event.values.joinToString(",", "[", "]")} }"
-    }
+object AccelerometerEventEmitter {
+    val tag = this.javaClass.name
 
-    override fun listen(callback: (Any) -> Unit) {
-        val sensor = manager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+    fun start(manager: SensorManager, processor: PublishProcessor<Any>) {
         val listener = object : SensorEventListener {
             override fun onSensorChanged(event: SensorEvent?) {
                 if (event?.sensor?.getType() != Sensor.TYPE_ACCELEROMETER) {
                     return
                 }
 
-                callback(AccelerometerEvent(event))
+                Log.d(tag, "${Thread.currentThread().name}: emit event")
+                processor.onNext(AccelerometerEvent(event))
             }
 
             override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {
             }
         }
+        val sensor = manager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
         manager.registerListener(listener, sensor, SensorManager.SENSOR_DELAY_NORMAL)
     }
 }
